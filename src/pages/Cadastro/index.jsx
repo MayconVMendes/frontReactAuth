b9bb5daa2c2c80530027b0551705ee735e163634
Maycon <maycon-big@hotmail.com>
2024-09-11 13:58:35 -0300

    feat: using useRef to get values to input

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,12 +1,53 @@
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+
 function Cadastro() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(nameRef);
+  }
+
   return (
-    <div>
-      <h2>cadastro</h2>
-      <form action="">
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+    <div className=" max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+        Cadastro
+      </h2>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <input
+          ref={nameRef}
+          type="text"
+          placeholder="Nome"
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+        />
+        <input
+          ref={emailRef}
+          type="email"
+          placeholder="Email"
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+        />
+        <input
+          ref={passwordRef}
+          type="password"
+          placeholder="Senha"
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400"
+        >
+          Cadastrar
+        </button>
       </form>
+      <Link
+        to="/login"
+        className="text-blue-700 hover:underline mt-4 block text-center"
+      >
+        Já tem uma conta? Faça login
+      </Link>
     </div>
   );
 }
